e606abb4d27e051b82327e2b8ea6d58865235a03
yichienlu <yichien.lu89@gmail.com>
2024-04-19 17:48:38 +0800

    feat:__v

--- models/post.js
+++ models/post.js
@@ -22,6 +22,9 @@ const postSchema = new mongoose.Schema(
         type:Number,
         default:0
       }
+  },
+  {
+    versionKey:false
   }
 );
 const Post = mongoose.model("post", postSchema);
